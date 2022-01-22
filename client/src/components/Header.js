28073ccc1950f9f52efa5faf5f2af9382b6a94c9
Abiodun Samuel <abiodunsamyemi@gmail.com>
2022-01-22 08:33:48 +0100

    product

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Navbar, Container, Nav } from "react-bootstrap";
+import { LinkContainer } from "react-router-bootstrap";
 import { AiOutlineShoppingCart } from "react-icons/ai";
 import { AiOutlineLogin } from "react-icons/ai";
 
@@ -8,16 +9,22 @@ const Header = () => {
     <header>
       <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
         <Container>
-          <Navbar.Brand href="/">MyShop</Navbar.Brand>
+          <LinkContainer to="/">
+            <Navbar.Brand>MyShop</Navbar.Brand>
+          </LinkContainer>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="ms-auto">
-              <Nav.Link href="/cart" className="d-flex align-items-center">
-                <AiOutlineShoppingCart className="mx-1" /> Cart
-              </Nav.Link>
-              <Nav.Link href="/login" className="d-flex align-items-center">
-                <AiOutlineLogin className="mx-1" /> Login
-              </Nav.Link>
+              <LinkContainer to="/cart">
+                <Nav.Link className="d-flex align-items-center">
+                  <AiOutlineShoppingCart className="mx-1" /> Cart
+                </Nav.Link>
+              </LinkContainer>
+              <LinkContainer to="/login">
+                <Nav.Link className="d-flex align-items-center">
+                  <AiOutlineLogin className="mx-1" /> Login
+                </Nav.Link>
+              </LinkContainer>
             </Nav>
           </Navbar.Collapse>
         </Container>
---
+++ client/src/components/Rating.js
@@ -0,0 +1,68 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { IoIosStarHalf } from "react-icons/io";
+import { IoIosStarOutline } from "react-icons/io";
+import { IoIosStar } from "react-icons/io";
+
+const Rating = ({ value, text, color }) => {
+  return (
+    <div className="rating">
+      <span style={{ color: color }}>
+        {value >= 1 ? (
+          <IoIosStar />
+        ) : value >= 0.5 ? (
+          <IoIosStarHalf />
+        ) : (
+          <IoIosStarOutline />
+        )}
+      </span>
+      <span style={{ color: color }}>
+        {value >= 2 ? (
+          <IoIosStar />
+        ) : value >= 1.5 ? (
+          <IoIosStarHalf />
+        ) : (
+          <IoIosStarOutline />
+        )}
+      </span>
+      <span style={{ color: color }}>
+        {value >= 3 ? (
+          <IoIosStar />
+        ) : value >= 2.5 ? (
+          <IoIosStarHalf />
+        ) : (
+          <IoIosStarOutline />
+        )}
+      </span>
+      <span style={{ color: color }}>
+        {value >= 4 ? (
+          <IoIosStar />
+        ) : value >= 3.5 ? (
+          <IoIosStarHalf />
+        ) : (
+          <IoIosStarOutline />
+        )}
+      </span>
+      <span style={{ color }}>
+        {value >= 5 ? (
+          <IoIosStar />
+        ) : value >= 4.5 ? (
+          <IoIosStarHalf />
+        ) : (
+          <IoIosStarOutline />
+        )}
+      </span>
+      <span>{text && text}</span>
+    </div>
+  );
+};
+Rating.defaultProps = {
+  color: "#f8e825",
+};
+Rating.propTypes = {
+  value: PropTypes.number.isRequired,
+    text: PropTypes.string.isRequired,
+    color: PropTypes.string,
+};
+
+export default Rating;
