a0841e634847a1aeac763cdd6f5e24dc359ee50c
Abiodun Samuel <abiodunsamyemi@gmail.com>
2022-02-02 07:15:20 +0100

    mongoose setup

--- server/server.js
+++ server/server.js
@@ -1,7 +1,9 @@
 import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./config/db.js";
-import products from "./data.js";
+import colors from "colors";
+import productsRoutes from "./routes/productRoutes.js";
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 dotenv.config();
 
 connectDB();
@@ -10,14 +12,10 @@ const app = express();
 app.get("/", (req, res) => {
   res.send("api is runung");
 });
+app.use("/api/products", productsRoutes);
+app.use(notFound);
+app.use(errorHandler);
 
-app.get("/api/products", (req, res) => {
-  res.json(products);
-});
-app.get("/api/products/:id", (req, res) => {
-  const product = products.find((p) => p._id === req.params.id);
-  res.json(product);
-});
 const PORT = process.env.PORT || 5000;
 app.listen(
   PORT,
