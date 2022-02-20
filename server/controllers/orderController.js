801e29d8ebb5f55baf9766a7c107bb4740b77272
Abiodun Samuel <abiodunsamyemi@gmail.com>
2022-02-20 21:30:30 +0100

    order details section

--- server/controllers/orderController.js
+++ server/controllers/orderController.js
@@ -33,4 +33,20 @@ const addOrderItems = asyncHandler(async (req, res) => {
     res.status(201).json({ message: "order has been created" });
   }
 });
-export { addOrderItems };
+
+//@desc get order by id
+//@route get api/orders/:id
+//@access private
+const getOrderById = asyncHandler(async (req, res) => {
+  const order = await Order.findById(req.params.id).populate(
+    "user",
+    "name email"
+  );
+  if (order) {
+    res.json(order);
+  } else {
+    res.status(404);
+    throw new Error("Order not found!");
+  }
+});
+export { addOrderItems, getOrderById };
